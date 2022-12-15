fa95601347c8aa1e540709ec1ebb0c0a7cadcc17
Carlos Renato <64719682+car-lo-s@users.noreply.github.com>
2022-12-15 12:51:31 -0300

    Add files via upload

--- dado.js
+++ dado.js
@@ -1,10 +1,22 @@
 info = [
-    {
-    "titulo":"teste",
-    "texto":"um dois tres teste"
-    },
-    {
-        "titulo":"feijao",
-        "texto":"um dois tres teste"
-        }
-]
+  {
+    titulo: "Cinema-Cinema",
+    texto: "Projeto praticar front e buscar uma forma mais pratica de adcionar filmes/series por meio de um arquivo com o nome a descrição e o endereço da imagem.",
+    img:"img/cinema-cinema.png"
+  },
+  {
+    titulo: "Landing page Psico",
+    texto: "Uma página estática para praticar front-end.",
+    img:"img/page psico.png"
+  },
+  {
+    titulo: "Lista de Tarefas",
+    texto: "Projeto feito para praticar a construção de um CRUD e criar uma lista de tarefas com um custo atrelado, acima de um determinado valor, a linha do item muda a cor.",
+    img:"img/crud-teste.png"
+  },
+  {
+    titulo: "CEP",
+    texto: "Projeto feito para praticar o uso de API.",
+    img:"img/CEP.png"
+  },
+];
--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let card = document.querySelectorAll('.projeto-card')
+window.onload = ()=>{ 
+    let area = document.querySelector('.projeto-area')
+    
 
-for(var i = 0; i<card.length;i++){
-    card[i].addEventListener("mouseover",()=>{
-        
+    info.map((a)=>{
+        let cartao = document.querySelector('.projeto-card').cloneNode(true)
+        cartao.querySelector('.projeto-img img').setAttribute('src',a.img)
+        cartao.querySelector('.projeto-nome').innerHTML = "Nome: "+ a.titulo
+        cartao.querySelector('.projeto-descricao').innerHTML = "Descrição: "+ a.texto
+        area.append(cartao) 
+       
     })
+
+    let card = document.querySelectorAll('.projeto-card')
+    card.forEach((item)=>{
+        item.addEventListener('mouseover',()=>{
+            item.querySelector('.projeto-dado').style.display = "flex";
+            item.querySelector('.projeto-img').style.display = "none";
+        })
+        item.addEventListener('mouseout',()=>{
+            item.querySelector('.projeto-dado').style.display = "none";
+            item.querySelector('.projeto-img').style.display = "flex";
+        })
+    });
+
 }
